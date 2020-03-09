317102b7ee6384292e27eb089bef222c94584e6c
Yorguin Murillo <ymurillo2@ncstudents.niagaracollege.ca>
2020-03-08 23:47:44 -0400

    Added web server to the weather app

---
+++ app.js
@@ -0,0 +1,26 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+const input = process.argv[2]
+
+if(!input){
+  console.log('Please provide search criteria')
+} else{
+  geocode(input, (error, {latitude, longitude, location}) => {
+    if(error){
+      return console.log(error)
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if(error){
+        return console.log(error)
+      }
+      console.log(location)
+      console.log(forecastData.message)         
+      })  
+  }) 
+}
+
+
+
+
+
+  
